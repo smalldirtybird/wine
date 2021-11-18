4143662c366f8a826997587bd4d864da5249c16e
Andrey Sokoloff <smalldirtybird@gmail.com>
2021-11-18 20:49:38 +0300

    added template rendering from xslx-sheet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
+import pandas
+from pprint import pprint
 
 
 def get_word_type_of_age(age):
@@ -21,12 +23,20 @@ def get_age_of_winery():
     return result
 
 
+def get_wine_data_from_excel(filepath):
+    excel_data_df = pandas.read_excel(filepath, na_values=['N/A', 'NA', 'NaN', 'nan'], keep_default_na=False)
+    return excel_data_df.to_dict('records')
+
+
 if __name__ == '__main__':
+    wines = get_wine_data_from_excel('wine.xlsx')
+    pprint(wines)
     env = Environment(loader=FileSystemLoader('.'),
                       autoescape=select_autoescape(['html', 'xml']))
     template = env.get_template('template.html')
     rendered_page = template.render(
-        age_of_winery=get_age_of_winery()
+        age_of_winery=get_age_of_winery(),
+        wines=wines
     )
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
