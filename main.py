cf483d2f65f16ca3272dad4896146bbadd31a5c3
Andrey Sokoloff <smalldirtybird@gmail.com>
2021-11-18 22:44:49 +0300

    update main.py - sort drinks by categories

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
+import collections
 from pprint import pprint
 
 
@@ -18,18 +19,24 @@ def get_word_type_of_age(age):
 def get_age_of_winery():
     year_of_foundation = 1920
     years = str(datetime.date.today().year - year_of_foundation)
-    word_type = get_word_type_of_age(years)
-    result = f'Уже {years} {word_type} с вами'
+    word_type_of_age = get_word_type_of_age(years)
+    result = f'Уже {years} {word_type_of_age} с вами'
     return result
 
 
-def get_wine_data_from_excel(filepath):
-    excel_data_df = pandas.read_excel(filepath, na_values=['N/A', 'NA', 'NaN', 'nan'], keep_default_na=False)
-    return excel_data_df.to_dict('records')
+def get_wines_categories(filepath):
+    drinks_from_excel = pandas.read_excel(
+        filepath, na_values=['N/A', 'NA', 'NaN', 'nan'], keep_default_na=False)
+    drinks_info = drinks_from_excel.to_dict('records')
+    wines_by_categories = collections.defaultdict(list)
+    for drink in drinks_info:
+        category = drink['Категория']
+        wines_by_categories[category].append(drink)
+    return wines_by_categories
 
 
 if __name__ == '__main__':
-    wines = get_wine_data_from_excel('wine.xlsx')
+    wines = get_wines_categories('wine2.xlsx')
     pprint(wines)
     env = Environment(loader=FileSystemLoader('.'),
                       autoescape=select_autoescape(['html', 'xml']))
